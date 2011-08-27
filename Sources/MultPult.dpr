cd01f57967657891008ba0eaf29c361eae3f8879
Nashev <nashev@mail.ru>
2011-08-28 02:37:39 +0400

    Sources folder

---
+++ Sources/MultPult.dpr
@@ -0,0 +1,16 @@
+program MultPult;
+
+uses
+  Forms,
+  MainFormUnit in 'MainFormUnit.pas' {MainForm},
+  AVICompression in '..\Downloads\avi_work\AVICompression.pas',
+  AVIFile32 in '..\Downloads\avi_work\AVIFile32.pas',
+  vfw in '..\Downloads\avi_work\vfw.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
